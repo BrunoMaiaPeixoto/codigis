844f7e0c96b6664529b63c1bdc599299bdc1e296
BrunoMaiaPeixoto <brunomaiapeixoto@hotmail.com>
2024-09-08 19:49:40 -0300

    Continuação e funcionalidades novas

--- Alura class/app.js
+++ Alura class/app.js
@@ -1,1 +1,24 @@
-console.log(dados);
+function pesquisar(){
+    let section = document.getElementById("resultados-pesquisa")
+console.log(section);
+let resultados = ""
+
+for (let dado of dados) {
+    resultados += ` <div class="item-resultado">
+    <h2>
+     ${dado.epoca}
+    </h2>
+    <p class="descricao-meta">${dado.descricao} 
+    <a href=${dado.link} target="_blank">Mais informações</a>
+    </div>
+    `
+}
+
+section.innerHTML = resultados
+}
+
+// console.log(section);
+
+
+
+  
